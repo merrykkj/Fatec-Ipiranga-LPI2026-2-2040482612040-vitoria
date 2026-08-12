89ed79743d910d176453414d3561bd3d3f99289c
Aluno <aluno@IPIRANGA.FATEC>
2026-08-12 16:00:52 -0300

    feat: implementa exercicio 03 (nullpointerexception e autounboxing)

---
+++ LPI-Atividade-N1-1/LP-Atividade-01-03-2040482612040.java
@@ -0,0 +1,26 @@
+
+import java.util.Scanner;
+
+public class LP-Atividade-01-03-2040482612040 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Integer idade = null;
+
+        try {
+            System.out.println("Tentando realizar operação aritmÉtica com Integer nulo...");
+            idade = idade + 5;
+
+        } catch (NullPointerException e) {
+            System.out.println("Erro Capturado com sucesso: Não foi possível calcular porque a idade não foi informada (null).");
+            System.out.println("Detalhe da exceção: " + e);
+        }
+
+        System.out.print("Informe uma idade válida: ");
+        idade = scanner.nextInt();
+
+        System.out.println("Reatribuindo valor válido para a variável...");
+        System.out.println("Operação bem-sucedida! Idade atual: " + idade + " | Idade no próximo ano: " + ( idade + 1));
+    }
+
+}
